13f330e5f0b75f18423a1ff2d89c0575195b9609
Vika Kolomiets <vikakolomiets@gmail.com>
2023-07-12 23:39:23 +0300

    create Login LoginPage

---
+++ src/test/java/tests/FirstTests.java
@@ -0,0 +1,22 @@
+package tests;
+
+import org.testng.annotations.Test;
+import utils.ApiUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FirstTests {
+
+    //private static final String KB_DASHBOARD_PAGE_URL = "http://localhost/kanboard/?controller=DashboardController&action=show";
+    private static final String BASE_URL = "http://127.0.0.1/kanboard/";
+    private static final String BASE_URL_local = "http://localhost/dashboard/";
+    @Test
+    public final void testGetCookie() {
+        Map<String, String> cookies = ApiUtils.getCookie(BASE_URL);
+        System.out.println(cookies.values());
+
+    }
+
+
+}
--- src/test/java/utils/ApiUtils.java
+++ src/test/java/utils/ApiUtils.java
@@ -7,6 +7,10 @@ import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.jsoup.Jsoup;
 
+import java.io.UnsupportedEncodingException;
+import java.net.http.HttpHeaders;
+import java.util.Arrays;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,10 +18,10 @@ import static io.restassured.RestAssured.given;
 
 public class ApiUtils {
 
+    //private static final String BASE_URL = "http://127.0.0.1/kanboard/";
+    private static final String BASE_URL = "http://127.0.0.1/";
 
-    private static final String BASE_URL = "http://127.0.0.1/kanboard/";
-
-    public static Map<String, String> getCookie() {
+    public static Map<String, String> getCookie(String url) {
         RestAssured.requestSpecification = new RequestSpecBuilder().setBaseUri("api").setContentType(ContentType.JSON)
                 .build().log().all();
 
@@ -25,9 +29,10 @@ public class ApiUtils {
                 .queryParam("controller", "AuthController")
                 .queryParam("action", "login")
                 .when()
-                .get(BASE_URL);
+                .get(url);
 
-        String cSRFToken = Jsoup.parseBodyFragment(response01.body().asString())
+        String string01 = response01.body().asString();
+        String cSRFToken = Jsoup.parseBodyFragment(string01)
                 .getElementsByAttributeValue("name", "csrf_token").attr("value");
 
         String cookieKBSID = response01.getCookie("KB_SID");
@@ -35,7 +40,8 @@ public class ApiUtils {
 
         Response response02 = given()
                 .config(RestAssured.config()
-                        .encoderConfig(EncoderConfig.encoderConfig().encodeContentTypeAs("x-www-form-urlencoded", ContentType.URLENC)))
+                        .encoderConfig(EncoderConfig.encoderConfig()
+                                .encodeContentTypeAs("x-www-form-urlencoded", ContentType.URLENC)))
                 .contentType("application/x-www-form-urlencoded; charset=UTF-8")
                 .formParam("remember_me", "1")
                 .formParam("username", "admin")
@@ -45,7 +51,7 @@ public class ApiUtils {
                 .queryParam("action", "check")
                 .cookie("KB_SID", cookieKBSID)
                 .when()
-                .post(BASE_URL);
+                .post(url);
 
         String setCookieHeaderValue = response02.header("Set-Cookie");
         Map<String, String> cookieKBRMMap = new HashMap<String, String>();
@@ -58,5 +64,26 @@ public class ApiUtils {
         cookies.put("KB_SID", cookieKBSID);
         cookies.put("KB_RM", cookieKBRMMap.get("KB_RM"));
         return cookies;
+    }
+    private static String getToken() {
+
+        Response postRequest = RestAssured.given().
+                auth().
+                basic("admin", "admin").
+                header("Content-Type", "application/json").
+                //header("Cookie", "KB_SID=sufseo930noe2qbr1smedqumpe").
+                and().
+                body("{\"jsonrpc\": \"2.0\", \"method\": \"getMyProjects\", \"id\": 1}").
+                post("http://localhost:80/jsonrpc.php");
+        // postRequest.prettyPrint();
+        String s = postRequest.asString();
+        return s;
+    }
+
+    public static void main(String[] args) {
+        //Map<String, String> temp = ApiUtils.getCookie(BASE_URL);
+        String temp =getToken();
+        System.out.println(temp);
+
     }
 }
