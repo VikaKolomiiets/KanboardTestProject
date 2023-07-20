ea1ea228ff22bd2aa6c262225092bddcec302a26
Vika Kolomiets <vikakolomiets@gmail.com>
2023-07-20 15:28:33 +0300

    update dashboardPage by elements for ProjectTests

--- src/main/java/pages/DashboardPage.java
+++ src/main/java/pages/DashboardPage.java
@@ -11,24 +11,46 @@ import static com.codeborne.selenide.Selenide.*;
 @Getter
 public class DashboardPage {
     private SelenideElement pageTitle = $(".title-container");
-
     private SelenideElement title = $(".title");
     private SelenideElement avatarDropDown = $(".avatar-letter");
+    private SelenideElement projectCreateRef = $("[class=page-header] a[href='/project/create']");
+    private SelenideElement requestWindowTitle = $x("//h2[contains(text(), 'project')]");
+    private SelenideElement inputNameFormProject = $("#form-name");
+    private SelenideElement inputIdentifierFormProject = $("#form-identifier");
+    private SelenideElement inputTaskLimitFormProject = $("#form-task_limit");
+    private SelenideElement checkBoxFormProject = $x("//*[contains(@name, 'swimlane')]");
+    private SelenideElement buttonSubmitFormProject  = $("//button[@type='submit']");
+    private SelenideElement cancelRefFormProject = $x("//a[text()='cancel']");
 
     public SelenideElement getLogoutRef() {
         List<SelenideElement> logoutList = $$("a[href='/logout']");
         return logoutList.get(1);
     }
-
-
-
     public String getTitleContainerName(){
         return pageTitle.shouldBe(Condition.visible).getText();
     }
-
     public LoginPage logOutFromDashboardPage(){
         avatarDropDown.shouldBe(Condition.visible).click();
         getLogoutRef().shouldBe(Condition.visible).click();
         return new LoginPage();
     }
+    public DashboardPage fillInNewProjectForm(String projectName, String identifier, Integer taskLimit){
+        inputNameFormProject.sendKeys(projectName);
+        inputIdentifierFormProject.sendKeys(identifier);
+        inputTaskLimitFormProject.sendKeys(taskLimit.toString());
+        checkBoxFormProject.shouldBe(Condition.visible).click();
+        return this;
+    }
+    public DashboardPage openNewProjectRequestForm(){
+        projectCreateRef.shouldBe(Condition.visible).click();
+        requestWindowTitle.shouldBe(Condition.exactValue("New project"));
+        return this;
+    }
+    public void clickSaveButtonNewProjectForm(){
+        buttonSubmitFormProject.shouldBe(Condition.visible).click();
+    }
+    public void clickCancelInNewProjectForm(){
+        cancelRefFormProject.shouldBe(Condition.visible).click();
+    }
+
 }
--- src/test/java/api/tests/project/ProjectTests.java
+++ src/test/java/api/tests/project/ProjectTests.java
@@ -4,14 +4,19 @@ import api.enums.UserRole;
 import api.steps.ProjectApiSteps;
 import api.steps.UserApiSteps;
 import api.tests.BaseTest;
+import io.qameta.allure.Description;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.DashboardPage;
+import pages.LoginPage;
 
 public class ProjectTests extends BaseTest {
     private static final String USERNAME = "Bossy Girl";
     private static final String PASSWORD = "It's_Pass";
     private ProjectApiSteps projectApiSteps = new ProjectApiSteps();
     private UserApiSteps userApiSteps = new UserApiSteps();
+    private DashboardPage dashboardPage;
     private String userId;
 
     @BeforeMethod
@@ -19,6 +24,16 @@ public class ProjectTests extends BaseTest {
         userId = userApiSteps.createUser(USERNAME, PASSWORD);
         boolean isManager = userApiSteps.updateUserRole(Integer.valueOf(userId), UserRole.APP_MANAGER);
         System.out.println(userId);
+        DashboardPage dashboardPage = new LoginPage()
+                .openLoginPage()
+                .setUserNameInput(USERNAME)
+                .setPasswordInput(PASSWORD)
+                .openDashBoardPageByClickOnSubmitButton();
+    }
+
+    @Description("Positive")
+    @Test
+    public void testCreateNewProject(){
 
     }
 
