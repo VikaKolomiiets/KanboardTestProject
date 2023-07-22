183c4c54cb4abd7bf8045b542e95a653475c996d
Vika Kolomiets <vikakolomiets@gmail.com>
2023-07-22 12:07:34 +0300

    added setUpMethod and tearDownMethod

--- src/test/java/api/tests/project/RemoveProject.java
+++ src/test/java/api/tests/project/RemoveProject.java
@@ -1,9 +1,15 @@
 package api.tests.project;
 
+import api.enums.UserRole;
 import api.steps.ProjectApiSteps;
 import api.steps.UserApiSteps;
 import api.tests.BaseTest;
+import io.qameta.allure.Description;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 import pages.DashboardPage;
+import pages.LoginPage;
 
 public class RemoveProject extends BaseTest {
 
@@ -16,7 +22,32 @@ public class RemoveProject extends BaseTest {
     private UserApiSteps userApiSteps = new UserApiSteps();
     private DashboardPage dashboardPage;
     private String userId;
+    private String actualTitle;         //for negative test
     private String projectId;
-    
+    @BeforeMethod
+    public void setUpMethod(){
+        userId = userApiSteps.createUser(USERNAME, PASSWORD);
+        boolean isManager = userApiSteps.updateUserRole(Integer.valueOf(userId), UserRole.APP_MANAGER);
+        System.out.println("UserId = " + userId);
+        projectId = projectApiSteps.createProject(PROJECT_NAME, Integer.valueOf(userId));
+        System.out.println("Project Id = " + projectId);
+    }
+
+    @Description("Positive: Remove project by owner of this project")
+    @Test
+    public void testRemoveProject(){
+        actualTitle = this.dashboardPage.getTitle().getText();
+    }
+
+    @AfterMethod
+    public void tearDownMethod(){
+        //for negative tests
+        if(actualTitle.contains(PROJECT_NAME)){
+            String projectId = projectApiSteps.getProjectIdByName(PROJECT_NAME, USERNAME, PASSWORD);
+            boolean isRemovedProject = projectApiSteps.removeProject(Integer.valueOf(projectId),USERNAME, PASSWORD);
+        }
+        boolean isRemovedUser = userApiSteps.removeUser(Integer.valueOf(userId));
+    }
+
 
 }
