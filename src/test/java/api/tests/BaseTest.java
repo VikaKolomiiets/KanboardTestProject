5b1d66f094974d5bffd7eb66f10166d1d8c730c6
Vika Kolomiets <vikakolomiets@gmail.com>
2023-07-24 20:09:39 +0300

    crossbrawsertest done

--- src/test/java/api/tests/BaseTest.java
+++ src/test/java/api/tests/BaseTest.java
@@ -1,20 +1,46 @@
 package api.tests;
 
+import com.codeborne.selenide.Browsers;
 import com.codeborne.selenide.Configuration;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import com.codeborne.selenide.WebDriverRunner;
+import org.openqa.selenium.devtools.v85.domsnapshot.model.ArrayOfStrings;
+import org.testng.annotations.*;
+import org.testng.util.Strings;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static api.utils.EnvProperties.BASE_URL;
 import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
 
+
 public class BaseTest {
-    @BeforeClass
-    public void setUp() {
+
+
+
+//    @DataProvider(name = "data-provider")
+//    public Object[][] dpMethod(){
+//        return new Object[][] {{"chrome"}, {"edge"}, {"none"}};
+//    }
+
+    @BeforeTest
+    public void setUpTests(){
         Configuration.baseUrl = BASE_URL;
     }
 
+    @BeforeClass
+    @Parameters({"Brawser type"})
+    public void setUpClass(String brawserType) {
+        Configuration.browser = brawserType;
+    }
+
     @AfterClass(alwaysRun = true)
-    public void cleanup() {
+    public void tearDownClass() {
         closeWebDriver();
+    }
+
+    @AfterTest
+    public void tearDownTests(){
+        System.out.println("All browsers were tested");
     }
 }
